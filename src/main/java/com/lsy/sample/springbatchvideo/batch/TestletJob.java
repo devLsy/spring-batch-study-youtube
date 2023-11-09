c525b25f3ece958099bdfc247d0a98acebc4de91
이상엽 <infightersy@naver.com>
2023-11-09 13:16:58 +0900

    step 2 작성

--- src/main/java/com/lsy/sample/springbatchvideo/batch/TestletJob.java
+++ src/main/java/com/lsy/sample/springbatchvideo/batch/TestletJob.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +24,7 @@ public class TestletJob {
     public Job taskletJob_batchBuild() {
         return jobBuilderFactory.get("taskletjob")
                 .start(taskletJob_step())
+                .next(taskletJob_step2(null))
                 .build();
     }
 
@@ -30,6 +33,16 @@ public class TestletJob {
         return stepBuilderFactory.get("taskletJob_step1")
                 .tasklet((a, b) -> {
                     log.info(" ======> job =====> [step1]");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    @JobScope
+    public Step taskletJob_step2(@Value("#{jobParameters[date]}") String date) {
+        return stepBuilderFactory.get("taskletJob_step2")
+                .tasklet((a, b) -> {
+                    log.info(" ======> [step1] =====> [step2] [{}]", date);
                     return RepeatStatus.FINISHED;
                 }).build();
     }
